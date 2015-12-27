9f0dbe8c066306f716b960306e0dfe5b3f95cbd3
inouexyz <inouexyz@gmail.com>
2015-12-27 19:16:31 +0900

    リンクをクリックすると音の出るiOSアプリ(swift v2.1対応)

--- soundBtn/ViewController.swift
+++ soundBtn/ViewController.swift
@@ -7,9 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player:AVAudioPlayer!
 
+    @IBAction func btnTap(sender: AnyObject) {
+        let path = NSBundle.mainBundle().pathForResource("sample", ofType: "mp3")!
+        let url = NSURL.fileURLWithPath(path)
+        
+        // AVAudioPlayerのインスタンス化
+        do{
+            player = try AVAudioPlayer(contentsOfURL: url, fileTypeHint: nil)
+        }catch{
+            print("Failed AVAudioPlayer Instance")
+        }
+        
+        // 再生
+        player.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
