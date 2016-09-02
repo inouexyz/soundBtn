9aa9ac091dd1e7e53b740e1d7f015ec150b7a54f
inouexyz <inouexyz@gmail.com>
2016-09-02 18:33:39 +0900

    へぇボタンの連打に対応

--- soundBtn/ViewController.swift
+++ soundBtn/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by inouexyz on 20151115.
 //  Copyright © 2015-2016 inouexyz. All rights reserved.
 //
-//  Latest edited by inouexyz on 20160729
+//  Latest edited by inouexyz on 20160902
 
 import UIKit
 import AVFoundation
@@ -13,21 +13,43 @@ import AVFoundation
 class ViewController: UIViewController,AVAudioPlayerDelegate {
     
     var player:AVAudioPlayer!
+    var player2:AVAudioPlayer!
+    var player3:AVAudioPlayer!
+    var i = 0
     
     @IBAction func btnTap(sender: AnyObject) {
         let path = NSBundle.mainBundle().pathForResource("hee", ofType: "mp3")!
         let url = NSURL.fileURLWithPath(path)
         
-        // AVAudioPlayerのインスタンス化
-        do{
-            player = try AVAudioPlayer(contentsOfURL: url, fileTypeHint: nil)
-        }catch{
-            print("Failed AVAudioPlayer Instance")
+        if(i==0){
+            do{
+                player = try AVAudioPlayer(contentsOfURL: url, fileTypeHint: nil)
+                player.play()
+            }catch{
+                print("Failed AVAudioPlayer Instance1")
+            }
+            i += 1
+            
+        } else if(i==1) {
+            do{
+                player2 = try AVAudioPlayer(contentsOfURL: url, fileTypeHint: nil)
+                player2.play()
+            }catch{
+                print("Failed AVAudioPlayer Instance2")
+            }
+            i += 1
+            
+        } else if(i==2) {
+            do{
+                player3 = try AVAudioPlayer(contentsOfURL: url, fileTypeHint: nil)
+                player3.play()
+            }catch{
+                print("Failed AVAudioPlayer Instance3")
+            }
+            i = 0
+            
         }
-        
-        // 再生
-        player.play()
-        
+        //player.play()
     }
     
     override func viewDidLoad() {
